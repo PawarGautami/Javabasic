174e6bdf1aebcd4ccbb460762218b09d0d73805d
PawarGautami <110668074+PawarGautami@users.noreply.github.com>
2022-08-08 11:16:57 +0530

    Add files via upload

---
+++ arithmatic.java
@@ -0,0 +1,16 @@
+class arithmatic
+{
+public static void main(String args[])
+{
+int a=110, b=20, c=150;
+
+System.out.println("A+B="+(a+b));
+System.out.println("A-B="+(a-b));
+System.out.println("A*B="+(a*b));
+float d;
+d=(float) a/b;
+System.out.println("A/B="+d);
+System.out.println("A%B="+(a%b));
+
+}
+}
